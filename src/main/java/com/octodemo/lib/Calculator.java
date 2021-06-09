6ecaf5fbc1c03641cd1df1a3a3e1262609ec85f5
Tugdual Grall <tgrall@github.com>
2021-06-09 10:50:04 +0200

    new call

--- src/main/java/com/octodemo/lib/Calculator.java
+++ src/main/java/com/octodemo/lib/Calculator.java
@@ -1,11 +1,18 @@
 package com.octodemo.lib;
 
+import jdk.javadoc.internal.doclets.formats.html.resources.standard;
+
 /**
  * Hello world!
  * for TC
  */
 public class Calculator
 {
+
+
+     String demo = "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";
+
+
     public int add(int a, int b) {
         return a + b;
     }
