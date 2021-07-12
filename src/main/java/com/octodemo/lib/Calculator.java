34905ae1d61e483d1209491853e924f07e064847
Tugdual Grall <tgrall@github.com>
2021-07-12 12:29:30 +0200

    add cloud calls

--- src/main/java/com/octodemo/lib/Calculator.java
+++ src/main/java/com/octodemo/lib/Calculator.java
@@ -6,6 +6,9 @@ package com.octodemo.lib;
  */
 public class Calculator
 {
+    
+     String demoSara = "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";
+    
     public int add(int a, int b) {
         return a + b;
     }
